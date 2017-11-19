4dc13b6f807647827ca7be7c9f1779b884d99422
tdzanski <tdzanski@gmail.comgit config --global user.name tdzanskigit config --listgit configgit config --global user.email tdzanski@gmail.com>
2017-11-18 22:36:07 -0600

    completed version of Lexical Scoping assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix loads a supplied matrix into memory
+## cacheSolve returns the inverse of the supplied matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix() creates an object that stores a matrix and its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve returns the inverse of x using the cached matrix in the makeCacheMatrix's enviornment
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    ## Return a matrix that is the inverse of 'x'
+    m
 }
+
+#functions to create matrcies to test makeCacheMatrix and cacheSolve
+testmake1 <- function() {
+    m1 <<- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+}
+
+testmake2 <- function() {
+    n2 <<- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2)
+}
+
